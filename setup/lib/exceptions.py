928c45d1a011b30f43cca39ff4a7089e274ebf0a
Rondineli <rondineli.gomes.araujo@gmail.com>
2016-07-19 12:30:56 +0100

    initi code

---
+++ setup/lib/exceptions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class ExpectedAWSKeys(Exception):
+	pass
+
+
+class ExpectedAWSRoles(Exception):
+	pass
+
+class ExpectedSubnetsAndVPC(Exception):
+	pass
+
+
+class ParameterProblems(Exception):
+	pass
---
+++ setup/lib/opswork_setup.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+
+import boto
+import uuid
+import json
+
+from boto import ec2, vpc, opsworks
+from boto.exception import EC2ResponseError
+
+from etc import settings
+from exceptions import (
+    ExpectedAWSKeys,
+    ExpectedAWSRoles,
+    ExpectedSubnetsAndVPC,
+    ParameterProblems
+)
+
+class OpsWorkSetup(object):
+
+    def __init__(self, access_key=None, secret_key=None):
+        if not settings.access_key or not settings.secret_key:
+            self.access_key = access_key
+            self.secret_key = secret_key
+        else:
+            self.access_key = settings.access_key
+            self.secret_key = settings.secret_key
+
+        if self.access_key is None or self.secret_key is None:
+            raise ExpectedAWSKeys(
+                "Please, provide a secret key and acces key aws, see: http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html"
+            )
+
+    @property
+    def conn(self):
+        _conn = opsworks.connect_to_region(
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
+            region_name='us-east-1'
+        )
+        return _conn
+
+    @property
+    def security_groups(self):
+        _security_groups = ec2.connect_to_region(
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
+            region_name=settings.region
+        )
+        return _security_groups
+    
+    @property
+    def describe_subnets(self):
+        _describe_subnets = vpc.connect_to_region(
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key,
+            region_name=settings.region
+        )
+        return _describe_subnets
+    
+
+    def create_stack(self):
+        """ create stack for modeling environment """
+        stack_name = 'ElasticSearchStack-{}'.format(str(uuid.uuid4())[:8])
+        if (not settings.default_instance_profile_arn or settings.default_instance_profile_arn is None
+            or not settings.service_role_arn or settings.service_role_arn is None):
+            raise ExpectedAWSRoles("Please, provide the correct services roles, see http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create.html and check README.md about the access required for this roles.")
+
+        self.stack = self.conn.create_stack(
+            name=stack_name,
+            region=settings.region,
+            default_availability_zone=settings.available_zone,
+            custom_json="{}".format(settings.custom_json_cheff),
+            use_custom_cookbooks=True,
+            hostname_theme='Europe_Cities',
+            use_opsworks_security_groups=True,
+            custom_cookbooks_source={"Type": "git", "Url": settings.repository_url},
+            default_instance_profile_arn=settings.default_instance_profile_arn,
+            service_role_arn=settings.service_role_arn,
+            default_ssh_key_name=settings.ssh_key_name_default,
+            default_os=settings.default_os,
+            configuration_manager=settings.configuration_manager
+        )
+        return self.stack
+
+    def create_security_group(self, network_policies=[]):
+        """ get security groups and modeling template for security groups"""
+        security_groups = self.security_groups.get_all_security_groups()
+        for sg_attributes in security_groups:
+            if u"AWS-OpsWorks-Default-Server" == sg_attributes.name:
+                for new_rulee in network_policies:
+                    try:
+                        sg_attributes.authorize(
+                            new_rule['protocol'],
+                            new_rule['from_port'],
+                            new_rule['to_port'],
+                            new_rule['cidr_ip']
+                        )
+                    except EC2ResponseError:
+                        print "Specified Rule already exists...skipped"
+                        pass
+
+                # If put rule with "0.0.0.0/0" will be deleted.
+                # I decided put this code here, just to force no to have world rule for anywhere
+                for rule in sg_attributes.rules:
+                    for grant in rule.grants: 
+                        if u"0.0.0.0/0" == grant.cidr_ip:
+                            sg_attributes.revoke(
+                                rule.ip_protocol,
+                                rule.from_port,
+                                rule.to_port,
+                                grant.cidr_ip
+                            )
+
+    def vpc_data_network(self, protocol='tcp', cidr_ips=[]):
+        if not cidr_ips:
+            # Get default subnets on defauilt VPC (my case)
+            subnets = self.describe_subnets.get_all_subnets()
+            network_policies = []
+            for subnet in subnets:
+                cidr_ips.append(subnet.cidr_block)
+            network_policies = [{
+                'protocol': 'tcp',
+                'from_port': 9300,
+                'to_port': 9300,
+                'cidr_ip': cidr_ips[0]
+            }, {
+                'protocol': 'tcp',
+                'from_port': 9201,
+                'to_port': 9201,
+                'cidr_ip': cidr_ips[1]
+            }, {
+                'protocol': 'tcp',
+                'from_port': 80,
+                'to_port': 80,
+                'cidr_ip': cidr_ips[1]
+            }]
+        else:
+            for cidr_ip in cidr_ips:
+                network_policies.append({
+                    cidr_ip['protocol'],
+                    cidr_ip['from_port'],
+                    cidr_ip['to_port'],
+                    cidr_ip['cidr_ip']
+                })
+        if not network_policies:
+            raise ExpectedSubnetsAndVPC("Well, in this case, it's necessary to create one VPC and two subnets for this region")
+        self.create_security_group(network_policies=network_policies)
+
+    def create_layer(self, new_stack=False, stack_id=None):
+        layer_name = 'ElasticSearchLayer-{}'.format(str(uuid.uuid4())[:8])
+        if new_stack and stack_id is None:
+            new_stack_id = self.create_stack()['StackId']
+        if stack_id:
+            new_stack_id = stack_id
+
+        self.stack['StackId'] = new_stack_id
+
+        self.layer_created = self.conn.create_layer(
+            stack_id=self.stack['StackId'],
+            type='custom',
+            name=layer_name,
+            volume_configurations=settings.ebs_volum,
+            shortname='elasticsearchlayer',
+            custom_instance_profile_arn=settings.default_instance_profile_arn,
+            auto_assign_elastic_ips=True,
+            custom_recipes=settings.recipes
+        )
+        return self.layer_created
+
+    def create_instances(self, number_instances=3, subnets_list=[], new_layer=True, new_stack=True, layer_id=[], **kwargs):
+        if new_layer and not layer_id:
+            print "if.."
+            new_layer_id = [self.create_layer(new_stack=new_stack)['LayerId']]
+        if layer_id:
+            new_layer_id = layer_id
+
+        if subnets_list:
+            if len(subnets_list) != number_instances:
+                raise ParameterProblems("number instances and subnets_list needed the same lenght.")
+        else:
+            subnets_list=None
+
+        for loop in range(0, number_instances):
+            if subnets_list:
+                new_subnets_list = subnets_list[loop]
+            else:
+                new_subnets_list = None
+
+            instance_created = self.conn.create_instance(
+                stack_id=self.stack['StackId'],
+                layer_ids=new_layer_id,
+                root_device_type='ebs',
+                instance_type=settings.instance_type,
+                subnet_id=new_subnets_list,
+                **kwargs
+            )
+            print instance_created
+            self.conn.start_instance(instance_created['InstanceId'])
+
+    def managament_instance(self, instance_id, action='stop'):
+        status = None
+        if action == 'stop':
+            status = self.conn.stop_instance(instance_id)
+        if action == 'start':
+            status = self.conn.start_instance(instance_id)
+        if action == 'delete':
+            status = self.conn.delete_instance(instance_id=instance_id, delete_elastic_ip=True, delete_volumns=True)
+
+        if not status:
+            raise UnrecognizedComand("Plase, try again with: 'stop', 'start', or 'delete' command.")
+        return status
